8d10316e89a2afcaef0d902e2030ea09c77e31f9
Brian Schlining <bschlining@gmail.com>
2017-08-10 13:24:43 -0700

    initial import. Modified from module in VARS

---
+++ src/main/xcode/avimagesnap/AVFStillImageCapture.m
@@ -0,0 +1,168 @@
+//
+//  AVFStillImageCapture.m
+//  avimagesnap
+//
+// https://developer.apple.com/library/ios/qa/qa1702/_index.html
+// http://www.benjaminloulier.com/posts/ios4-and-direct-access-to-the-camera/
+// http://stackoverflow.com/questions/1320988/saving-cgimageref-to-a-png-file
+//
+//  Created by Brian Schlining on 11/21/13.
+//  Copyright (c) 2013 Brian Schlining. All rights reserved.
+//
+
+#import "AVFStillImageCapture.h"
+
+@implementation AVFStillImageCapture
+
+@synthesize session;
+@synthesize videoInput;
+@synthesize stillImageOutput;
+
+
+-(NSArray *) videoCaptureDevices {
+    [self initSession];
+    NSArray *devices = [AVCaptureDevice devices];
+    NSMutableArray *videoDevices = [NSMutableArray array];
+    for (AVCaptureDevice *device in devices) {
+        if ([device hasMediaType:AVMediaTypeVideo]) {
+            [videoDevices addObject:device];
+        }
+    }
+    return videoDevices;
+};
+
+-(NSArray *) videoCaptureDevicesAsStrings {
+    [self initSession];
+    NSArray *videoDevices = [self videoCaptureDevices];
+    NSMutableArray *localizedNames = [NSMutableArray array];
+    for (AVCaptureDevice *device in videoDevices) {
+        [localizedNames addObject:[device localizedName]];
+    }
+    return localizedNames;
+};
+
+-(AVCaptureDevice *) videoCaptureDeviceNamed: (NSString *)name {
+    [self initSession];
+    AVCaptureDevice *namedDevice = nil;
+    NSArray *videoDevices = [self videoCaptureDevices];
+    for (AVCaptureDevice *device in videoDevices) {
+        NSLog(@"Examining video capture device '%@'", [device localizedName]);
+        if ([[device localizedName] isEqualToString:name]) {
+            namedDevice = device;
+        }
+    }
+    if (namedDevice == Nil) {
+        NSLog(@"Did not find avideo input named '%@'", name);
+    }
+    return namedDevice;
+};
+
+-(void) initSession {
+    if (session == nil) {
+        session = [[AVCaptureSession alloc] init];
+        session.sessionPreset = AVCaptureSessionPresetPhoto;
+        [session startRunning];
+    }
+}
+
+-(void)  setupCaptureSessionUsingNamedDevice: (NSString *) name {
+    
+    [self initSession];
+    
+    if (session != nil) {
+
+        [session beginConfiguration];
+
+        if (videoInput != nil) {
+            [session removeInput:videoInput];
+            videoInput = nil;
+        }
+        
+        if (stillImageOutput != nil) {
+            [session removeOutput:stillImageOutput];
+            stillImageOutput = nil;
+        }
+        
+        AVCaptureDevice *device = [self videoCaptureDeviceNamed:name];
+        if (device != nil) {
+            NSError *error = nil;
+            videoInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+            if (videoInput != nil) {
+                [session addInput:videoInput];
+                
+                // create and configure output
+                stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+                [session addOutput:stillImageOutput];
+            }
+            else {
+                NSLog(@"Unable to create a video input from the device named '%@'", name);
+            }
+            
+        }
+        else {
+            NSLog(@"Unable to open video device named '%@", name);
+        }
+        
+        [session commitConfiguration];
+
+        
+        // First framegrab on DeckLink is always black. So just grab one and don't do anything with it.
+        AVCaptureConnection *captureConnection = nil;
+        if (stillImageOutput != nil) {
+            captureConnection = [[stillImageOutput connections] objectAtIndex:0];
+            [stillImageOutput captureStillImageAsynchronouslyFromConnection:captureConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+                [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            }];
+        }
+            
+    }
+}
+
+
+-(void) saveStillImageToPath:(NSString *)path {
+    AVCaptureConnection *captureConnection = nil;
+    
+    if (stillImageOutput != nil) {
+        captureConnection = [[stillImageOutput connections] objectAtIndex:0];
+    }
+    
+    if (captureConnection != nil) {
+        
+        [stillImageOutput captureStillImageAsynchronouslyFromConnection:captureConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+            
+            // Save as PNG
+            if (imageDataSampleBuffer != NULL) {
+                NSLog(@"Saving %@", path);
+                NSData *jpgData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+//                [jpgData writeToFile:path atomically:YES]; // Save as JPG
+                NSBitmapImageRep *bitmap = [NSBitmapImageRep imageRepWithData:jpgData];
+                NSData *pngData = [bitmap representationUsingType: NSPNGFileType properties:nil];
+                [pngData writeToFile:path atomically:YES]; // Save as PNG
+            }
+        }];
+    }
+}
+
+-(void) dealloc {
+
+    [session beginConfiguration];
+
+    if (videoInput != nil) {
+        [session removeInput:videoInput];
+        videoInput = nil;
+    }
+    
+    if (stillImageOutput != nil) {
+        [session removeOutput:stillImageOutput];
+        stillImageOutput = nil;
+    }
+
+    [session commitConfiguration];
+
+    [session stopRunning];
+    
+}
+
+@end
+
+
---
+++ src/main/xcode/avimagesnap/vars_avfoundation_AVFImageCaptureServiceImpl.m
@@ -0,0 +1,100 @@
+#include "vars_avfoundation_AVFImageCaptureServiceImpl.h"
+#include "AVFStillImageCapture.h"
+#import <JavaNativeFoundation/JavaNativeFoundation.h>
+
+
+AVFStillImageCapture *imageCapture = nil;
+
+void initImageCapture() {
+    if (imageCapture == nil) {
+        imageCapture = [[AVFStillImageCapture alloc] init];
+        [imageCapture initSession];
+    }
+}
+
+/*
+ * Class:     vars_avfoundation_AVFImageCaptureServiceImpl
+ * Method:    videoDevicesAsStrings
+ * Signature: ()[Ljava/lang/String;
+ */
+JNIEXPORT jobjectArray JNICALL Java_vars_avfoundation_AVFImageCaptureServiceImpl_videoDevicesAsStrings
+(JNIEnv *env, jobject clazz) {
+    
+    initImageCapture();
+	
+	// Grab the array of NSStrings
+	NSArray *videoDevicesAsStrings = [imageCapture videoCaptureDevicesAsStrings];
+	
+	// Look for the class for Java String
+	jclass stringClass = (*env)->FindClass(env, "Ljava/lang/String;");
+	
+	// The array to return
+	jobjectArray result = (*env)->NewObjectArray(env, [videoDevicesAsStrings count], stringClass, NULL);
+	
+	// Loop over devices
+	for (int i = 0; i < [videoDevicesAsStrings count]; i++) {
+		// Set the object in the java array to the device name converted to java string
+		(*env)->SetObjectArrayElement(env, result, i, JNFNSToJavaString(env, [videoDevicesAsStrings objectAtIndex:i]));
+	}
+	
+	// Return the result
+	return result;
+
+};
+
+/*
+ * Class:     vars_avfoundation_AVFImageCaptureServiceImpl
+ * Method:    startSessionWithNamedDevice
+ * Signature: (Ljava/lang/String;)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_vars_avfoundation_AVFImageCaptureServiceImpl_startSessionWithNamedDevice
+(JNIEnv *env, jobject clazz, jstring namedDevice) {
+	
+	// Convert the incoming Device Name to an NSString
+	NSString *device = JNFJavaToNSString(env, namedDevice);
+    
+    initImageCapture();
+
+    [imageCapture setupCaptureSessionUsingNamedDevice: device];
+	
+	// Convert the filename back to jstring for return
+	return JNFNSToJavaString(env, device);
+	
+};
+
+/*
+ * Class:     vars_avfoundation_AVFImageCaptureServiceImpl
+ * Method:    saveSnapshotToSpecifiedPath
+ * Signature: (Ljava/lang/String;)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_vars_avfoundation_AVFImageCaptureServiceImpl_saveSnapshotToSpecifiedPath
+(JNIEnv *env, jobject clazz, jstring specifiedPath) {
+	
+	// Convert the incoming filename to an NSString
+	NSString *path = JNFJavaToNSString(env, specifiedPath);
+	
+    if (imageCapture != nil) {
+        [imageCapture saveStillImageToPath:path];
+    }
+    else {
+        NSLog(@"Still image capture has not been configured. Unable to save %@", path);
+    }
+	
+	// Convert the filename back to jstring for return
+	return JNFNSToJavaString(env, path);
+
+};
+
+/*
+ * Class:     vars_avfoundation_AVFImageCaptureServiceImpl
+ * Method:    stopSession
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vars_avfoundation_AVFImageCaptureServiceImpl_stopSession
+(JNIEnv *env, jobject clazz) {
+	
+	// [imageCapture dealloc]; // Don't need. This project is using ARC
+    imageCapture = nil; // On deallocation the session will be terminated
+};
+
+
